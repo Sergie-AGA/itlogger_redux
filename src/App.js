4f18692c6e2d8e3e5948deef4d01e21f421a6ac3
Sergie <sergiearruda@gmail.com>
2021-04-10 09:22:26 +0100

    Creates Searchbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
+import SearchBar from "./components/layout/SearchBar";
+
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
@@ -8,7 +10,11 @@ const App = () => {
     // Initializes Materialize JS
     M.AutoInit();
   });
-  return <div className="App">My App</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+    </Fragment>
+  );
 };
 
 export default App;
--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const SearchBar = () => {
-  return <div></div>;
+  return (
+    <nav style={{ marginBottom: "30px" }}>
+      <div className="nav-wrapper">
+        <form action="">
+          <div className="input-field">
+            <input type="search" id="search" required />
+            <label class="label-icon" htmlFor="search">
+              <i className="material-icons">search</i>
+            </label>
+          </div>
+          <i className="material-icons">close</i>
+        </form>
+      </div>
+    </nav>
+  );
 };
 
 export default SearchBar;
